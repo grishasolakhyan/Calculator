c6f5f0fccb593520a78316327dcd986cc67ec5a6
grishasolakhyan <borsch.tv@mail.ru>
2024-08-06 00:58:59 +1000

    Calculator update

--- calculator.py
+++ calculator.py
@@ -59,15 +59,18 @@ class Ui_MainWindow(object):
         self.button_8 = btn_object.number_button(41, 40, 40, 40, 'btn 8', cw)
         self.button_9 = btn_object.number_button(82, 40, 40, 40, 'btn 9', cw)
         self.button_0 = btn_object.number_button(0, 163, 40, 40, 'btn 0', cw)
+        self.button_pi = btn_object.number_button(164, 163, 40, 40, 'btn pi', cw)
+        self.button_e = btn_object.number_button(205, 163, 40, 40, 'btn e', cw)
+
+        self.button_dot = btn_object.number_button(0, 204, 40, 40, 'btn dot', cw)
+        self.button_bracket1 = btn_object.number_button(164, 204, 40, 40, 'btn bracket 1', cw)
+        self.button_bracket2 = btn_object.number_button(205, 204, 40, 40, 'btn bracket 2', cw)
 
         self.button_plus = btn_object.number_button(123, 40, 40, 40, 'btn plus', cw)
         self.button_minus = btn_object.number_button(123, 81, 40, 40, 'btn minus', cw)
         self.button_multy = btn_object.number_button(123, 122, 40, 40, 'btn multy', cw)
         self.button_divid = btn_object.number_button(123, 163, 40, 40, 'btn divid', cw)
-        self.button_dot = btn_object.number_button(0, 204, 40, 40, 'btn dot', cw)
         self.button_degree = btn_object.number_button(123, 204, 40, 40, 'btn degree', cw)
-        self.button_bracket1 = btn_object.number_button(164, 204, 40, 40, 'btn bracket 1', cw)
-        self.button_bracket2 = btn_object.number_button(205, 204, 40, 40, 'btn bracket 2', cw)
 
         # self.button_sin = btn_object.number_button(164, 40, 40, 40, 'btn sin', cw)
         # self.button_cos = btn_object.number_button(164, 81, 40, 40, 'btn cos', cw)
@@ -75,9 +78,6 @@ class Ui_MainWindow(object):
         # self.button_asin = btn_object.number_button(205, 40, 40, 40, 'btn asin', cw)
         # self.button_acos = btn_object.number_button(205, 81, 40, 40, 'btn acos', cw)
         # self.button_atg = btn_object.number_button(205, 122, 40, 40, 'btn atg', cw)
-        #
-        # self.button_pi = btn_object.number_button(164, 163, 40, 40, 'btn pi', cw)
-        # self.button_e = btn_object.number_button(205, 163, 40, 40, 'btn e', cw)
 
         self.button_equal = btn_object.number_button(41, 163, 81, 40, 'btn equal', cw)
         self.button_clean = btn_object.number_button(41, 204, 81, 40, 'btn clean', cw)
@@ -103,15 +103,18 @@ class Ui_MainWindow(object):
         self.button_8.setText(_translate("MainWindow", "8"))
         self.button_9.setText(_translate("MainWindow", "9"))
         self.button_0.setText(_translate("MainWindow", "0"))
+        self.button_pi.setText(_translate("MainWindow", "π"))
+        self.button_e.setText(_translate("MainWindow", "e"))
+
+        self.button_dot.setText(_translate("MainWindow", "."))
+        self.button_bracket1.setText(_translate("MainWindow", "("))
+        self.button_bracket2.setText(_translate("MainWindow", ")"))
 
         self.button_plus.setText(_translate("MainWindow", "+"))
         self.button_minus.setText(_translate("MainWindow", "-"))
         self.button_multy.setText(_translate("MainWindow", "*"))
         self.button_divid.setText(_translate("MainWindow", "/"))
-        self.button_dot.setText(_translate("MainWindow", "."))
         self.button_degree.setText(_translate("MainWindow", "^"))
-        self.button_bracket1.setText(_translate("MainWindow", "("))
-        self.button_bracket2.setText(_translate("MainWindow", ")"))
 
         # self.button_sin.setText(_translate("MainWindow", "sin"))
         # self.button_cos.setText(_translate("MainWindow", "cos"))
@@ -119,9 +122,6 @@ class Ui_MainWindow(object):
         # self.button_asin.setText(_translate("MainWindow", "asin"))
         # self.button_acos.setText(_translate("MainWindow", "acos"))
         # self.button_atg.setText(_translate("MainWindow", "atg"))
-        #
-        # self.button_pi.setText(_translate("MainWindow", "π"))
-        # self.button_e.setText(_translate("MainWindow", "e"))
 
         self.button_equal.setText(_translate("MainWindow", "="))
         self.button_clean.setText(_translate("MainWindow", "C"))
@@ -137,6 +137,12 @@ class Ui_MainWindow(object):
         self.button_8.clicked.connect(lambda: self.write_number(self.button_8.text()))
         self.button_9.clicked.connect(lambda: self.write_number(self.button_9.text()))
         self.button_0.clicked.connect(lambda: self.write_number(self.button_0.text()))
+        self.button_pi.clicked.connect(lambda: self.write_number(self.button_pi.text()))
+        self.button_e.clicked.connect(lambda: self.write_number(self.button_e.text()))
+
+        self.button_dot.clicked.connect(lambda: self.write_number(self.button_dot.text()))
+        self.button_bracket1.clicked.connect(lambda: self.write_number(self.button_bracket1.text()))
+        self.button_bracket2.clicked.connect(lambda: self.write_number(self.button_bracket2.text()))
 
         self.button_plus.clicked.connect(lambda: self.write_math_symbol(self.button_plus.text()))
         self.button_minus.clicked.connect(lambda: self.write_math_symbol(self.button_minus.text()))
@@ -144,19 +150,12 @@ class Ui_MainWindow(object):
         self.button_divid.clicked.connect(lambda: self.write_math_symbol(self.button_divid.text()))
         self.button_degree.clicked.connect(lambda: self.write_math_symbol(self.button_degree.text()))
 
-        self.button_dot.clicked.connect(lambda: self.write_number(self.button_dot.text()))
-        self.button_bracket1.clicked.connect(lambda: self.write_number(self.button_bracket1.text()))
-        self.button_bracket2.clicked.connect(lambda: self.write_number(self.button_bracket2.text()))
-
         # self.button_sin.clicked.connect(self.btn_sinus)
         # self.button_cos.clicked.connect(self.btn_cosinus)
         # self.button_tg.clicked.connect(self.btn_tangens)
         # self.button_asin.clicked.connect(self.btn_asinus)
         # self.button_acos.clicked.connect(self.btn_acosinus)
         # self.button_atg.clicked.connect(self.btn_atangens)
-        #
-        # self.button_pi.clicked.connect(self.num_pi)
-        # self.button_e.clicked.connect(self.num_e)
 
         self.button_equal.clicked.connect(self.results)
         self.button_clean.clicked.connect(self.clean_results)
@@ -253,6 +252,21 @@ class Ui_MainWindow(object):
             if len(num_list) == 1:
                 final_result = num_list[0]
                 break
+
+            if 'π' or 'e' in n_list:
+                for i in range(len_n_list):
+                    if i == 0 and (n_list[i] == 'π' or n_list[i] == 'e') and (self.isnumeric(n_list[i+1]) == True or n_list[i+1] == 'π' or n_list[i+1] == 'e'):
+                            raise IncorrectExpression()
+                    elif (i > 0 and i < len_n_list-1) and (n_list[i] == 'π' or n_list[i] == 'e') and (self.isnumeric(n_list[i - 1]) == True or n_list[i - 1] == 'π' or n_list[i - 1] == 'e') or (self.isnumeric(n_list[i + 1]) == True or n_list[i + 1] == 'π' or n_list[i + 1] == 'e'):
+                            raise IncorrectExpression()
+                    elif i == len_n_list-1 and (n_list[i] == 'π' or n_list[i] == 'e') and (self.isnumeric(n_list[i-1]) == True or n_list[i-1] == 'π' or n_list[i-1] == 'e'):
+                            raise IncorrectExpression()
+                    else:
+                        if n_list[i] == 'π':
+                            n_list[i] = math.pi
+                        elif n_list[i] == 'e':
+                            n_list[i] = math.e
+
             for i in range(len_n_list - 1):
                 if '/' in n_list:  # если есть операция деления
                     if i > 0 and n_list[i] == '/' and self.isnumeric(n_list[i - 1]) == True and self.isnumeric(
@@ -353,11 +367,11 @@ class Ui_MainWindow(object):
         b_fnt_sze=12
         return b_fnt_sze
 
-    def num_pi(self):
-        self.label_result.setText(str(math.pi))
-
-    def num_e(self):
-        self.label_result.setText(str(math.e))
+    # def num_pi(self):
+    #     self.label_result.setText(str(math.pi))
+    #
+    # def num_e(self):
+    #     self.label_result.setText(str(math.e))
 
     def btn_sinus(self):
         pp=self.label_result.text()
